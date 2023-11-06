ed8938461f44c47df4059f5f73e0d1e2e7c3840d
bammietop03 <bammietop@gmail.com>
2023-11-07 00:30:01 +0100

    Store first object

---
+++ models/engine/file_storage.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+A class FileStorage that serializes instances to a JSON file and deserializes JSON file to instances
+"""
+import json
+from models.base_model import BaseModel
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        serialized = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, 'w', encoding='utf-8') as file:
+            json.dump(serialized, file)
+
+    def reload(self):
+        try:
+            with open(self.__file_path, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                for key, value in data.items():
+                    class_name, obj_id = key.split('.')
+                    obj = BaseModel(**value)
+                    self.__objects[key] = obj
+        except FileNotFoundError:
+            pass
