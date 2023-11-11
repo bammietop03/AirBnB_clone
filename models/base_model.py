25f5b46dfd59d92a90d7e1d9cf4fb210e61f65e7
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-11-11 14:09:14 -0800

    Updated the BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -81,8 +81,8 @@ class BaseModel:
         """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
+        obj_dict['created_at'] = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        obj_dict['updated_at'] = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
         return obj_dict
 
     @classmethod
