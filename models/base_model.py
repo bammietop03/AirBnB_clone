fb7712f598787f5cce68a4a331ce6b62624c3b99
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-11-11 15:10:32 -0800

    Updated the BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -51,7 +51,7 @@ class BaseModel:
                         self.__dict__[key] = self, key, datetime.strptime
                         (value, '%Y-%m-%dT%H:%M:%S.%f')
                     else:
-                        setattr(self, key, value)
+                        self.__dict__[key] = value
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
